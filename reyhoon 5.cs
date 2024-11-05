63dfb6020c454c799ac60dd392027c7ed2340009
Sasan2ex <sasan.hosseiny@yahoo.com>
2024-11-05 18:04:32 +0330

    Add files via upload

---
+++ reyhoon 5.cs
@@ -0,0 +1,22 @@
+﻿
+int n = 10;
+int m = 100;
+
+for (int i = n; i < m; i++)
+{
+    bool isprime = true; 
+    if (i < 2) isprime = false; 
+    else
+    {
+        for (int j = 2; j <i ; j++)
+        {
+            if (i % j == 0)
+            {
+                isprime = false;
+                break; 
+            }
+        }
+    }
+    if (isprime)
+        Console.WriteLine(i + " is prime");
+}
