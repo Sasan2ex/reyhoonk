502a35ee157feb7ae31e3219bb2b68192a947b50
Sasan2ex <sasan.hosseiny@yahoo.com>
2024-11-05 18:04:54 +0330

    Add files via upload

---
+++ reyhoon 6.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.Design;
+
+String input;
+
+Console.WriteLine("Are you fall in love?( Yes /NO) ");
+input = Console.ReadLine();
+
+if (input == "yes") 
+{
+    Console.WriteLine("you are luckY !");
+
+}
+if (input == "no") 
+    Console.WriteLine("Do you want to fall in love ?(Yes / No");
+    input = Console.ReadLine();
+
+if (input == "yes") 
+{
+    Console.WriteLine("If you want it , you will get it");
+}
+if (input == "no") 
+{
+    Console.WriteLine("It is your choice !");
+}
