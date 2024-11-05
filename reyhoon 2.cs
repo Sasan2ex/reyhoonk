a4a83c07c24309205d206d8e7f494ada8367bde5
Sasan2ex <sasan.hosseiny@yahoo.com>
2024-11-05 18:02:53 +0330

    Add files via upload

---
+++ reyhoon 2.cs
@@ -0,0 +1,27 @@
+﻿int n = 10;
+int m = 1000;
+
+for (int i = n; i < m; i++)
+{
+   
+    if (IsPalindrome(i))
+
+    {
+        Console.WriteLine(i);
+
+    }
+
+    static bool IsPalindrome(int number)
+    {
+        string str = number.ToString();
+        char[] arr = str.ToCharArray();
+        Array.Reverse(arr);
+        return str == new string(arr);
+    }
+}
+
+
+
+        
+       
+
