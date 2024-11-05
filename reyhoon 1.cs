88e78f9bac30208d6972088e91a016d66a270b35
Sasan2ex <sasan.hosseiny@yahoo.com>
2024-11-05 18:02:15 +0330

    Add files via upload

---
+++ reyhoon 1.cs
@@ -0,0 +1,32 @@
+﻿
+    
+        int n = 10;
+        int m = 100 ;
+
+        for (int i = n; i < m; i++)
+        {
+            int a = 0;
+            int b = 1;
+            int c = 0;
+            bool isFibo = false;
+
+           
+            while (c < m)
+            {
+                c = a + b;
+                if (n < c && c < m)
+                {
+                    if (i == c)
+                    {
+                        isFibo = true;
+                        break; 
+                    }
+                }
+                a = b;
+                b = c;
+            }
+
+            if (isFibo)
+                Console.WriteLine( i + " is a Fibonacci number");
+        }
+  
